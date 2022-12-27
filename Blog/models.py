e9fa61f77b29d73eb11a9d414bc3f76528c55e5e
matingafar530 <117345660+matingafar530@users.noreply.github.com>
2022-12-27 22:48:17 +0330

    adding blog&single blog models

--- Blog/models.py
+++ Blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class PostModel (models.Model):
+
+    PostImage=models.ImageField(upload_to= "postimage/",null=True)
+    PostTitle=models.CharField(max_length=70)
+    PostDescrption=models.CharField(max_length=400)
+    def __str__(self):
+        return self.PostTitle
+
+
+class SingelPostModel (models.Model):
+    Post = models.ForeignKey(PostModel , on_delete=models.CASCADE)
+    def __str__(self):
+        return self.PostTitle
