053062cf6a2b1460c610a493e14791be39b631e4
Amirali Golru <golruamirali@gmail.com>
2022-12-29 22:41:22 +0330

    contact us page added

--- Contact/views.py
+++ Contact/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
-# Create your views here.
+from django.http import HttpResponse
+from django.template import loader
+def contactus(request):
+    menutemplate = loader.get_template('Contact/contactus.html')
+    return HttpResponse(menutemplate.render())
--- autogallery/urls.py
+++ autogallery/urls.py
@@ -3,13 +3,15 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from Home.views import homepage, carshow , singlecar
+from Contact.views import contactus
 # from Products.views import carslist , singlecar
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', homepage),
     path('cars/', carshow),
-    path('cars/singlecar/<int:car_id>', singlecar)
+    path('cars/singlecar/<int:car_id>', singlecar),
+    path('contactus/' , contactus)
 ]
 
 
